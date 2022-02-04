d835c77425c85f5beb76aeb16c850891a448f4a6
MapkMapk <maybemark111@gmail.com>
2022-02-04 11:04:11 +0500

    Cоздание метода prevDate

---
+++ DateTimeLibTest/DateTimeClassTest.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using DateTimeLib;
+namespace DateTimeLibTest
+{
+    [TestClass]
+    public class DateTimeClassTest
+    {
+        [TestMethod]
+        public void PrevDate_CorrectDate_PrevDayReturn()
+        {
+            //Arrange
+            DateTime date = new DateTime(2008, 6, 1, 7, 47, 0);
+            DateTime expectedPrevDate = new DateTime(2008, 5, 31, 7, 47, 0);
+            //Act
+            DateTime actualPrevDate = DateTimeClass.PrevDate(date);
+
+            //Assert
+            Assert.AreEqual(expectedPrevDate, actualPrevDate);
+
+            
+        }
+    }
+}
---
+++ DateTimeLibTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DateTimeLibTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DateTimeLibTest")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("97e546f7-d86c-47eb-aa8d-a9d0e21025ab")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
